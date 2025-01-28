d6490b8d624d7b093297910d2b5187a277f0d759
Elias H. <elias.haqiqat@protonmail.com>
2025-01-28 04:55:41 +0100

    Revert "Add default Raylib program.cs"
    This reverts commit b46208331c38d5008be982ddb7a15498a9b8e6e4.

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -1,23 +1,3 @@
-﻿using Raylib_cs;
+﻿// See https://aka.ms/new-console-template for more information
 
-namespace Chip8;
-
-class Program
-{
-    public static void Main()
-    {
-        Raylib.InitWindow(800, 480, "Hello World");
-
-        while (!Raylib.WindowShouldClose())
-        {
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.White);
-
-            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
-
-            Raylib.EndDrawing();
-        }
-
-        Raylib.CloseWindow();
-    }
-}
+Console.WriteLine("Hello, World!");
