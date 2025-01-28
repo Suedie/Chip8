b46208331c38d5008be982ddb7a15498a9b8e6e4
Elias H. <elias.haqiqat@protonmail.com>
2025-01-28 02:59:49 +0100

    Add default Raylib program.cs

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Raylib_cs;
 
-Console.WriteLine("Hello, World!");
+namespace Chip8;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
