9bf203d4dd51d0cc139fc41d5679b530c844d0e5
Elias H. <elias.haqiqat@protonmail.com>
2025-01-28 22:43:16 +0100

    Attempt to add Raylib-Csharp

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Raylib_CSharp.Colors;
+using Raylib_CSharp.Rendering;
+using Raylib_CSharp.Windowing;
 
-Console.WriteLine("Hello, World!");
+namespace Chip8;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Window.Init(800, 600, "Hello World!");
+
+        while (!Window.ShouldClose())
+        {
+            Graphics.BeginDrawing();
+            Graphics.ClearBackground(Color.White);
+            
+            Graphics.DrawText("Hello, World!", 12, 12, 20, Color.Red);
+            
+            Graphics.EndDrawing();
+        }
+        
+        Window.Close();
+    }
+}
