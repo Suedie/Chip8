2a829a88425dac5fab570da247032b5c6a5534cb
Elias H. <elias.haqiqat@protonmail.com>
2025-01-28 01:58:15 +0100

    Declare memory in hex

--- Chip8/Memory.cs
+++ Chip8/Memory.cs
@@ -3,8 +3,8 @@ namespace Chip8;
 public class Memory
 {
     //Chip-8 has a 12 bit index register which means 4096 possible addresses in memory
-    //This translates to 4kB of ram
-    public int[] MemoryArray = new int[4096];
+    //This translates to 4kB of RAM
+    public int[] MemoryArray = new int[0x1000];
     
     
 }
