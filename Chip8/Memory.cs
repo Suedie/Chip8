96a07c2d97979eb55594dc720a05f82ece694c69
Elias H. <elias.haqiqat@protonmail.com>
2025-01-28 01:40:33 +0100

    Initial commit

---
+++ Chip8/Memory.cs
@@ -0,0 +1,10 @@
+namespace Chip8;
+
+public class Memory
+{
+    //Chip-8 has a 12 bit index register which means 4096 possible addresses in memory
+    //This translates to 4kB of ram
+    public int[] MemoryArray = new int[4096];
+    
+    
+}
