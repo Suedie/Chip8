37f8b297ab877853a195e635f4e8ec9742c2487e
Elias <elias.haqiqat@protonmail.com>
2025-01-30 03:11:07 +0100

    Add outline for decode operation

--- Chip8/Processor.cs
+++ Chip8/Processor.cs
@@ -7,17 +7,57 @@ class Processor {
 
     public Memory memory = new Memory();
 
-    public ushort Fetch() {
+    public uint Fetch() {
 
-        int part1 = memory.MemoryArray[PC];
+        uint part1 = memory.MemoryArray[PC];
         part1 = part1 << 4;
-        int opcode = part1 + memory.MemoryArray[PC + 1];
+        uint opcode = part1 + memory.MemoryArray[PC + 1];
 
         PC += 2;
 
-        return (ushort) opcode;
+        return opcode;
     }
 
-    
+    public void Decode(uint opcode) {
+        uint firstNibble = (opcode & 0b_1100_0000) >> 6;
+        uint secondNibble = (opcode & 0b_0011_0000) >> 4;
+        uint thirdNibble = (opcode & 0b_0000_1100) >> 2;
+        uint fourthNibble = opcode & 0b_0000_0011;
+
+        switch (firstNibble) {
+            case 0x0:
+            break;
+            case 0x1:
+            break;
+            case 0x2:
+            break;
+            case 0x3:
+            break;
+            case 0x4:
+            break;
+            case 0x5:
+            break;
+            case 0x6:
+            break;
+            case 0x7:
+            break;
+            case 0x8:
+            break;
+            case 0x9:
+            break;
+            case 0xA:
+            break;
+            case 0xB:
+            break;
+            case 0xC:
+            break;
+            case 0xD:
+            break;
+            case 0xE:
+            break;
+            case 0xF:
+            break;
+        }
+    }
 
 }
