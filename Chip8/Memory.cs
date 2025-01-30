e5f207693754b8dd90cdcc383bc6bed7b2067310
Elias <elias.haqiqat@protonmail.com>
2025-01-30 02:29:33 +0100

    Add LoadRom and LoadFont methods

--- Chip8/Memory.cs
+++ Chip8/Memory.cs
@@ -6,6 +6,22 @@ public class Memory
     //Each address is 1-byte
     //This translates to 4kB of RAM
     public byte[] MemoryArray = new Byte [0x1000];
+
+    //Puts the font data at 0x50 (80) which has become an unofficial standard
+    public void LoadFont() {
+        Chip8.Font _font = new();
+
+        byte[] _tmpFont = _font.TextFont;
+
+        MemoryArray.CopyTo(_tmpFont, 0x50);
+    }
     
-    
+    //Loads a rom into adress 0x200 (512)
+    //Some early Chip-8 games expect to be loaded into this address
+    public void LoadRom(string filepath) {
+        byte[] rom = File.ReadAllBytes(filepath);
+
+        MemoryArray.CopyTo(rom, 0x200);
+    }
+
 }
