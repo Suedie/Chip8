752e771983586198dc2aafe8e1b6c86eced4509b
Elias <elias.haqiqat@protonmail.com>
2025-01-30 06:31:18 +0100

    Make program run IBM logo

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -1,21 +1,29 @@
 ﻿using Raylib_CSharp.Colors;
 using Raylib_CSharp.Rendering;
 using Raylib_CSharp.Windowing;
+using Chip8;
 
 namespace Chip8;
 
 class Program
 {
+
     public static void Main(string[] args)
     {
         Window.Init(800, 600, "Hello World!");
+
+        Processor game = new Processor();
+        game.memory.LoadRom("/home/deck/vscodeprojects/Chip8/Chip8/roms/IBM Logo.ch8");
 
         while (!Window.ShouldClose())
         {
             Graphics.BeginDrawing();
             Graphics.ClearBackground(Color.White);
+
+            game.Decode(game.Fetch());
+            Console.WriteLine(game.display.PixelsToString());
             
-            Graphics.DrawText("Hello, World!", 12, 12, 20, Color.Red);
+            Graphics.DrawText(game.display.PixelsToString(), 0, 0, 20, Color.Red);
             
             Graphics.EndDrawing();
         }
