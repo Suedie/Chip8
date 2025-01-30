27511fc0bd556e9ef0399c61be3adbce969eb4cb
Elias <elias.haqiqat@protonmail.com>
2025-01-30 05:26:44 +0100

    Add opcodes for IBM

--- Chip8/Processor.cs
+++ Chip8/Processor.cs
@@ -9,6 +9,8 @@ class Processor {
 
     public Memory memory = new Memory();
 
+    public Display display = new Display();
+
     public uint Fetch() {
 
         uint part1 = memory.MemoryArray[PC];
@@ -85,7 +87,7 @@ class Processor {
     }
 
     private void ClearDisplay() {
-
+        Array.Clear(display.Pixels, 0, display.Pixels.Length);
     }
 
     private void Jump(uint NNN) {
@@ -105,7 +107,23 @@ class Processor {
     }
 
     private void DrawToDisplay(uint X, uint Y, uint N) {
-        
+        int posX = Registers[X] % 64;
+        int posY = Registers[Y] % 32;
+
+        Registers[0xF] = 0;
+
+        uint spriteRow = memory.MemoryArray[I];
+        for (uint h = 0; h < N || h + posY < display.Pixels.GetLength(0); h++) {
+            for (uint w = 0; w < 8 || w + posX < display.Pixels.GetLength(1); w++) {
+                uint pixel = spriteRow & (uint) Math.Pow(2, 7 - w);
+                if (pixel == 1 && display.Pixels[posY, posX+w] == 1) {
+                    display.Pixels[posY+w, posX+w] = 0;
+                    Registers[0xF] = 1;
+                } else if (pixel == 1) {
+                    display.Pixels[posY+w, posX+w] = 1;
+                }
+            }
+        }
     }
 
 }
