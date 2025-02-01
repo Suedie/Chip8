32cb9c60edcb0e09f2df9ff4c4699c2cc4179699
Elias <elias.haqiqat@protonmail.com>
2025-02-02 00:05:50 +0100

    Remove arguments from EmuLoop and DebugLoop

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -28,12 +28,12 @@ class Program
 
     private static Processor _game = new Processor(_memory, _display, _keypad, _delay, _sound);
 
-    public static string GamePath;
+    public static string? GamePath;
 
 
     public static void Main()
     {
-        GamePath = "/home/deck/vscodeprojects/Chip8/Chip8/roms/5-quirks.ch8";
+        GamePath = "../Chip8/roms/3-corax+.ch8";
         _game.LoadGame(GamePath);
         
         Raylib_CSharp.Time.SetTargetFPS(_targetFps);
@@ -42,77 +42,75 @@ class Program
 
         while (!Window.ShouldClose()) {
             if (!_debugEnabled) {
-                EmuLoop(_memory, _display, _keypad, _delay, _sound, _game, _accumulatedTime, _targetFps, _cyclesPerFrame);
+                EmuLoop();
             } else if (_debugEnabled) {
-                DebugLoop(_memory, _display, _keypad, _delay, _sound, _game, _accumulatedTime, _targetFps, _cyclesPerFrame);
+                DebugLoop();
             }
         }
 
         Window.Close();
     }
 
-    private static void EmuLoop(Memory memory, Display display, Keypad keypad, DelayTimer delayTimer, SoundTimer soundTimer, Processor game,
-     float accumulatedTime, int targetFps, float cyclesPerFrame) {
+    private static void EmuLoop() {
 
         Graphics.BeginDrawing();
         Graphics.ClearBackground(Color.White);
 
-        accumulatedTime += Raylib_CSharp.Time.GetFrameTime();
+        _accumulatedTime += Raylib_CSharp.Time.GetFrameTime();
 
         //If targetFPS == 60 then update at 60hz
-        while (accumulatedTime >= 1f / targetFps) {
-            delayTimer.Update(Raylib_CSharp.Time.GetFrameTime());
-            soundTimer.Update(Raylib_CSharp.Time.GetFrameTime());
+        while (_accumulatedTime >= 1f / _targetFps) {
+            _delay.Update(Raylib_CSharp.Time.GetFrameTime());
+            _sound.Update(Raylib_CSharp.Time.GetFrameTime());
 
             //CPU runs at 1000 Instructions per second
             //At 60 FPS that becomes 17 instructions per frame
-            for(int i = 0; i < cyclesPerFrame; i++) {
-                game.Decode(game.Fetch());
+            for(int i = 0; i < _cyclesPerFrame; i++) {
+                _game.Decode(_game.Fetch());
             }
-            accumulatedTime -= 1f / targetFps;
+            _accumulatedTime -= 1f / _targetFps;
 
             if (Input.IsKeyPressed(KeyboardKey.Backspace))
                 _debugEnabled = !_debugEnabled;
         }
 
-        DrawMatrix(game.GetScreenMatrix());
+        DrawMatrix(_game.GetScreenMatrix());
                 
         Graphics.EndDrawing();
     }
 
-    private static void DebugLoop(Memory memory, Display display, Keypad keypad, DelayTimer delayTimer, SoundTimer soundTimer, Processor game,
-     float accumulatedTime, int targetFps, float cyclesPerFrame) {
+    private static void DebugLoop() {
         
         Graphics.BeginDrawing();
         Graphics.ClearBackground(Color.White);
 
-        accumulatedTime += Raylib_CSharp.Time.GetFrameTime();
+        _accumulatedTime += Raylib_CSharp.Time.GetFrameTime();
 
-        while (accumulatedTime >= 1f / targetFps) {
+        while (_accumulatedTime >= 1f / _targetFps) {
             DebugControls();
             if (!_isPaused) {
-                delayTimer.Update(Raylib_CSharp.Time.GetFrameTime());
-                soundTimer.Update(Raylib_CSharp.Time.GetFrameTime());
+                _delay.Update(Raylib_CSharp.Time.GetFrameTime());
+                _sound.Update(Raylib_CSharp.Time.GetFrameTime());
 
-                for(int i = 0; i < cyclesPerFrame; i++) {
-                    game.Decode(game.Fetch());
+                for(int i = 0; i < _cyclesPerFrame; i++) {
+                    _game.Decode(_game.Fetch());
                 }
             } else if (_isPaused) { //Manually step through each opcode and print it
                 if (Input.IsKeyPressed(KeyboardKey.Space)) {
-                    delayTimer.Update(Raylib_CSharp.Time.GetFrameTime());
-                    soundTimer.Update(Raylib_CSharp.Time.GetFrameTime());
+                    _delay.Update(Raylib_CSharp.Time.GetFrameTime());
+                    _sound.Update(Raylib_CSharp.Time.GetFrameTime());
 
-                    game.Decode(game.Fetch());
-                    game.PrintCurrentOpcode();
+                    _game.Decode(_game.Fetch());
+                    _game.PrintCurrentOpcode();
                 }
             }
-            accumulatedTime -= 1f / targetFps;
+            _accumulatedTime -= 1f / _targetFps;
 
             if (Input.IsKeyPressed(KeyboardKey.Backspace))
                 _debugEnabled = !_debugEnabled;
         }
 
-        DrawMatrix(game.GetScreenMatrix());
+        DrawMatrix(_game.GetScreenMatrix());
                 
         Graphics.EndDrawing();
      }
