cc61ec9687f054bca7d0f1b6e4587e0187e94180
Elias <elias.haqiqat@protonmail.com>
2025-02-01 22:00:17 +0100

    Add simple debug mode with pause and play

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -2,6 +2,7 @@
 using Raylib_CSharp.Interact;
 using Raylib_CSharp.Rendering;
 using Raylib_CSharp.Windowing;
+using Raylib_CSharp.Audio;
 
 using Chip8.src;
 
@@ -10,6 +11,10 @@ namespace Chip8;
 class Program
 {
 
+    public static bool debugEnabled = true;
+    public static bool isPaused = false;
+
+    
     public static void Main()
     {
         float accumulatedTime = 0f;
@@ -17,10 +22,7 @@ class Program
         float targetCPUCyclesPerSecond = 1000f;
         float cyclesPerFrame = targetCPUCyclesPerSecond / (float) targetFps;
 
-        Window.Init(1280, 640, "CHIP-8");
-
-        IFont font = new Font();
-        Memory memory = new Memory(font);
+        Memory memory = new Memory(new Font());
         Display display = new Display();
         Keypad keypad = new Keypad();
 
@@ -28,37 +30,86 @@ class Program
         SoundTimer sound = new SoundTimer();
 
         Processor game = new Processor(memory, display, keypad, delay, sound);
-        game.LoadGame("/home/deck/vscodeprojects/Chip8/Chip8/roms/Delay Timer Test [Matthew Mikolay, 2010].ch8");
+        game.LoadGame("/home/deck/vscodeprojects/Chip8/Chip8/roms/5-quirks.ch8");
         
         Raylib_CSharp.Time.SetTargetFPS(targetFps);
 
+        Window.Init(1280, 640, "CHIP-8");
 
-        while (!Window.ShouldClose())
-        {
-            Graphics.BeginDrawing();
-            Graphics.ClearBackground(Color.White);
+        while (!Window.ShouldClose()) {
+           if (Input.IsKeyPressed(KeyboardKey.L))
+                debugEnabled = !debugEnabled;
 
-            accumulatedTime += Raylib_CSharp.Time.GetFrameTime();
+            if (!debugEnabled) {
+                EmuLoop(memory, display, keypad, delay, sound, game, accumulatedTime, targetFps, cyclesPerFrame);
+            } else if (debugEnabled) {
+                DebugLoop(memory, display, keypad, delay, sound, game, accumulatedTime, targetFps, cyclesPerFrame);
+            }
 
-            while (accumulatedTime >= 1f / targetFps) {
-            delay.Update(Raylib_CSharp.Time.GetFrameTime());
-            sound.Update(Raylib_CSharp.Time.GetFrameTime());
+        }
+
+        Window.Close();
+    }
+
+    private static void EmuLoop(Memory memory, Display display, Keypad keypad, DelayTimer delayTimer, SoundTimer soundTimer, Processor game,
+     float accumulatedTime, int targetFps, float cyclesPerFrame) {
+
+        Graphics.BeginDrawing();
+        Graphics.ClearBackground(Color.White);
+
+        accumulatedTime += Raylib_CSharp.Time.GetFrameTime();
+
+        while (accumulatedTime >= 1f / targetFps) {
+            delayTimer.Update(Raylib_CSharp.Time.GetFrameTime());
+            soundTimer.Update(Raylib_CSharp.Time.GetFrameTime());
+
+            for(int i = 0; i < cyclesPerFrame; i++) {
+                game.Decode(game.Fetch());
+            }
+            accumulatedTime -= 1f / targetFps;
+        }
+
+        DrawMatrix(game.GetScreenMatrix());
+                
+        Graphics.EndDrawing();
+    }
+
+    private static void DebugLoop(Memory memory, Display display, Keypad keypad, DelayTimer delayTimer, SoundTimer soundTimer, Processor game,
+     float accumulatedTime, int targetFps, float cyclesPerFrame) {
+        
+        Graphics.BeginDrawing();
+        Graphics.ClearBackground(Color.White);
+
+        accumulatedTime += Raylib_CSharp.Time.GetFrameTime();
+
+        while (accumulatedTime >= 1f / targetFps) {
+            if (Input.IsKeyPressed(KeyboardKey.P)) {
+                isPaused = !isPaused;
+            }
+            if (!isPaused) {
+                delayTimer.Update(Raylib_CSharp.Time.GetFrameTime());
+                soundTimer.Update(Raylib_CSharp.Time.GetFrameTime());
 
                 for(int i = 0; i < cyclesPerFrame; i++) {
                     game.Decode(game.Fetch());
                 }
-                accumulatedTime -= 1f / targetFps;
+            } else if (isPaused) {
+                if (Input.IsKeyPressed(KeyboardKey.Space)) {
+                    delayTimer.Update(Raylib_CSharp.Time.GetFrameTime());
+                    soundTimer.Update(Raylib_CSharp.Time.GetFrameTime());
+
+                    game.Decode(game.Fetch());
+                }
             }
-
-            DrawMatrix(game.GetScreenMatrix());
-            
-            Graphics.EndDrawing();
+            accumulatedTime -= 1f / targetFps;
         }
-        
-        Window.Close();
-    }
 
-    public static void DrawMatrix(byte[,] pixels) {
+        DrawMatrix(game.GetScreenMatrix());
+                
+        Graphics.EndDrawing();
+     }
+
+    private static void DrawMatrix(byte[,] pixels) {
 
         Graphics.BeginDrawing();
         Graphics.ClearBackground(Color.Black);
@@ -69,6 +120,13 @@ class Program
                     Graphics.DrawRectangle(i*20, j*20, 20, 20, Color.White);
                 }
             }
+        }
+    }
+
+    public static void DebugControls(bool debugEnabled) {
+
+        if (Input.IsKeyDown(KeyboardKey.P)) {
+
         }
     }
 }
