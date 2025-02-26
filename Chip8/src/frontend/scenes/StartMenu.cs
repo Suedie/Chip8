bb576a398b812ba510fdff95104aaa1d8f7c86e7
Elias <elias.haqiqat@protonmail.com>
2025-02-26 13:38:15 +0100

    Fix capitalisation

--- Chip8/src/frontend/scenes/StartMenu.cs
+++ Chip8/src/frontend/scenes/StartMenu.cs
@@ -15,7 +15,7 @@ class StartMenu : AbstractMenu
     public override Button[] CreateButtons()
     {
         Buttons = new Button[3];
-        Buttons[0] = new Button(0, 0, ButtonWidth, ButtonHeight, new SceneChangeClick("Load Game", SceneIdentifier.GameSelect));
+        Buttons[0] = new Button(0, 0, ButtonWidth, ButtonHeight, new SceneChangeClick("Load game", SceneIdentifier.GameSelect));
         Buttons[1] = new Button(0, 0, ButtonWidth, ButtonHeight, new SceneChangeClick("Options", SceneIdentifier.OptionsMenu));
         Buttons[2] = new Button(0, 0, ButtonWidth, ButtonHeight, new ExitClick("Exit to desktop"));
         AlignButtons();
